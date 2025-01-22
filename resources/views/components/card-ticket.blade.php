5575eb0d0cdee1f358e7addc53db98c7b3cce33c
daralams <strongpeople006@gmail.com>
2025-01-23 03:16:15 +0700

    update

--- resources/views/components/card-ticket.blade.php
+++ resources/views/components/card-ticket.blade.php
@@ -2,12 +2,12 @@
     @foreach ($tickets as $ticket)
     <div class="card w-100 mb-2">
         <div class="card-header d-flex justify-content-between">
-            <p>{{ $ticket["name"] }}</p>
-            <p>IDR {{ $ticket["price"]}}</p>
+            <p>{{ $ticket->name }}</p>
+            <p>IDR {{ $ticket->price_ekonomi}}</p>
         </div>
         <div class="card-body">
-          {{-- <h5 class="card-title">{{ $ticket["keberangkatan"]}} -> {{ $ticket["tujuan"]}}</h5>
-          <p class="card-text">{{ $ticket["start"]}} -> {{ $ticket["end"]}}</p> --}}
+          <h5 class="card-title">{{ $ticket->airport->take_off}} -> {{ $ticket->airport->landing}}</h5>
+          <h5 class="card-title">{{ $ticket->airport->take_off_time}} -> {{ $ticket->airport->landing_time}}</h5>
           <a href="/tiket/{{$ticket['id']}}" class="btn btn-primary">Pilih Tiket</a>
         </div>
       </div>
